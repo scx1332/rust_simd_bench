a91b4b108545dd856f1712311834eb723ffcb64b
scx1332 <sieciech.czajka@golem.network>
2022-12-28 01:47:28 +0100

    simd bench

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,35 @@
 use packed_simd::*;
 use std::time::Instant;
 
-const INTERNAL_LOOPS: i32 = 100;
+fn test_scalar(vec: &mut [f32]) {
+    for i in 0..vec.len() {
+        vec[i] = (vec[i] + vec[i]) / (vec[i] - 1.0f32);
+        vec[i] = (vec[i] + vec[i]) / (vec[i] - 1.0f32);
+        if vec[i] == -234.03 {
+            vec[i] += 0.01;
+        }
+    }
+}
+
+fn test_2(vec: &mut [f32]) {
+    const VECTOR_SIZE: usize = 2;
+    type VectorType = f32x2;
+
+    if vec.len() % VECTOR_SIZE != 0 {
+        panic!("Vector size must be a multiple of {}", VECTOR_SIZE);
+    };
+    let mut idx = 0;
+    loop {
+        let mut f = VectorType::from_slice_aligned(&vec[idx..idx + VECTOR_SIZE]);
+        f = (f + f) / (f - VectorType::splat(1.0));
+        f = (f + f) / (f - VectorType::splat(1.0));
+        f.write_to_slice_aligned(&mut vec[idx..idx + VECTOR_SIZE]);
+        idx += VECTOR_SIZE;
+        if idx >= vec.len() {
+            break;
+        }
+    }
+}
 
 fn test_4(vec: &mut [f32]) {
     const VECTOR_SIZE: usize = 4;
@@ -13,9 +41,48 @@ fn test_4(vec: &mut [f32]) {
     let mut idx = 0;
     loop {
         let mut f = VectorType::from_slice_aligned(&vec[idx..idx + VECTOR_SIZE]);
-        for _times in 0..INTERNAL_LOOPS {
-            f = (f + f) / (f - VectorType::splat(1.0));
+        f = (f + f) / (f - VectorType::splat(1.0));
+        f = (f + f) / (f - VectorType::splat(1.0));
+        f.write_to_slice_aligned(&mut vec[idx..idx + VECTOR_SIZE]);
+        idx += VECTOR_SIZE;
+        if idx >= vec.len() {
+            break;
         }
+    }
+}
+
+fn test_8(vec: &mut [f32]) {
+    const VECTOR_SIZE: usize = 8;
+    type VectorType = f32x8;
+
+    if vec.len() % VECTOR_SIZE != 0 {
+        panic!("Vector size must be a multiple of {}", VECTOR_SIZE);
+    };
+    let mut idx = 0;
+    loop {
+        let mut f = VectorType::from_slice_aligned(&vec[idx..idx + VECTOR_SIZE]);
+        f = (f + f) / (f - VectorType::splat(1.0));
+        f = (f + f) / (f - VectorType::splat(1.0));
+        f.write_to_slice_aligned(&mut vec[idx..idx + VECTOR_SIZE]);
+        idx += VECTOR_SIZE;
+        if idx >= vec.len() {
+            break;
+        }
+    }
+}
+
+fn test_16(vec: &mut [f32]) {
+    const VECTOR_SIZE: usize = 16;
+    type VectorType = f32x16;
+
+    if vec.len() % VECTOR_SIZE != 0 {
+        panic!("Vector size must be a multiple of {}", VECTOR_SIZE);
+    };
+    let mut idx = 0;
+    loop {
+        let mut f = VectorType::from_slice_aligned(&vec[idx..idx + VECTOR_SIZE]);
+        f = (f + f) / (f - VectorType::splat(1.0));
+        f = (f + f) / (f - VectorType::splat(1.0));
         f.write_to_slice_aligned(&mut vec[idx..idx + VECTOR_SIZE]);
         idx += VECTOR_SIZE;
         if idx >= vec.len() {
@@ -25,33 +92,87 @@ fn test_4(vec: &mut [f32]) {
 }
 
 fn main() {
-    let mut vec_input = Vec::new();
-    for _i in 0..100000 {
+    const TEST_LENGTH: usize = 10000000;
+    let mut vec_input = Vec::with_capacity(TEST_LENGTH);
+    for _i in 0..TEST_LENGTH {
         vec_input.push(rand::random::<f32>());
     }
 
     let mut vec = vec_input.clone();
-
-    let internal_loops = 100;
-    //process scalar
     let time_start = Instant::now();
-    for i in 0..vec.len() {
-        for _j in 0..internal_loops {
-            vec[i] = (vec[i] + vec[i]) / (vec[i] - 1.0f32);
-        }
-    }
+
+    test_scalar(&mut vec);
     let time_end = Instant::now();
 
     println!("scalar: {:?}", time_end - time_start);
-    println!("Result {:?}", vec.iter().sum::<f32>());
 
-    let mut vec = vec_input.clone();
-    test_4(&mut vec);
+    let res = vec.iter().sum::<f32>();
 
-    let time_start = Instant::now();
-    let time_end = Instant::now();
-    println!("simd: {:?}", time_end - time_start);
-    println!("Result {:?}", vec.iter().sum::<f32>());
+    {
+        let mut vec = vec_input.clone();
+        let jh = std::thread::spawn(move || {
+            let time_start = Instant::now();
+            test_2(&mut vec);
+            let time_end = Instant::now();
+
+            let res2 = vec.iter().sum::<f32>();
+            if res != res2 {
+                panic!("Results differ! {res} != {res2}");
+            }
+            time_end - time_start
+        });
+        let duration = jh.join().unwrap();
+        println!("simd 2 {:?}", duration);
+    }
+    {
+        let mut vec = vec_input.clone();
+        let jh = std::thread::spawn(move || {
+            let time_start = Instant::now();
+            test_4(&mut vec);
+            let time_end = Instant::now();
+
+            let res2 = vec.iter().sum::<f32>();
+            if res != res2 {
+                panic!("Results differ! {res} != {res2}");
+            }
+            time_end - time_start
+        });
+        let duration = jh.join().unwrap();
+        println!("simd 4 {:?}", duration);
+    }
+    {
+        let mut vec = vec_input.clone();
+        let jh = std::thread::spawn(move || {
+            let time_start = Instant::now();
+            test_8(&mut vec);
+            let time_end = Instant::now();
+
+            let res2 = vec.iter().sum::<f32>();
+            if res != res2 {
+                panic!("Results differ! {res} != {res2}");
+            }
+            time_end - time_start
+        });
+        let duration = jh.join().unwrap();
+        println!("simd 8 {:?}", duration);
+    }
+    {
+        let mut vec = vec_input.clone();
+        let jh = std::thread::spawn(move || {
+            //test 2
+            let time_start = Instant::now();
+            test_16(&mut vec);
+            let time_end = Instant::now();
+
+            let res2 = vec.iter().sum::<f32>();
+            if res != res2 {
+                panic!("Results differ! {res} != {res2}");
+            }
+            time_end - time_start
+        });
+        let duration = jh.join().unwrap();
+        println!("simd 16 {:?}", duration);
+    }
 
     //println!("{:?}", a + b);
 }
